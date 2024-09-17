53c5571df1b35d1126aa633c9075328e1f8c9743
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-17 20:16:40 -0300

    added big-card component styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ import { MenuTitleComponent } from './components/menu-title/menu-title.component
     SmallCardComponent,
     BigCardComponent,
     MenuTitleComponent,
+    HomeComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
