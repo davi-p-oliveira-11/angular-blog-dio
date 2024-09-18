e6fbab4af979f262c7d648f94d50723a3c829da9
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-17 22:43:51 -0300

    added menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { SmallCardComponent } from './components/small-card/small-card.component
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,7 @@ import { HomeComponent } from './pages/home/home.component';
     BigCardComponent,
     MenuTitleComponent,
     HomeComponent,
+    MenuBarComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
